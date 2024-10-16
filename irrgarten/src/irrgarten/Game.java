578cd34d9892d1c36876f3d9eaea176b01bab4dd
Usuario <sergiocalvogonzalez7@gmail.com>
2024-10-16 21:33:52 +0200

    Subo el archivo de la clase Game, con las cabeceras de los métodos

---
+++ irrgarten/src/irrgarten/Game.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package irrgarten;
+
+/**
+ *
+ * @author usuario
+ */
+public class Game {
+    private static int MAX_ROUNDS = 10;
+    private int currentPlayerIndex;
+    private String log;
+    
+    //Game(int nplayers)
+    
+    //public boolean finished()
+    
+    //public boolean nextStep(Directions preferredDirection)
+    
+    //public GameState getGameState()
+    
+    //private void configureLabyrinth()
+    
+    //private void nextPlayer()
+    
+    //private Directions actualDirection(Directions preferredDirection)
+    
+    //private GameCharacter combat(Monster monster)
+    
+    //private void manageReward(GameCharacter winner)
+    
+    //private void manageResurrection()
+    
+    //private void logPlayerWon()
+    
+    //private void logMonsterWon()
+    
+    //private void logResurrected()
+    
+    //private void logPlayerSkipTurn()
+    
+    //private void logPlayerNoOrderns()
+    
+    //private void logNoMonster()
+    
+    //provate void logRounds(int rounds, int max)
+}
