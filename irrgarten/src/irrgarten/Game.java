a84f3301708d6195a30adc7154f99a3a21fea1e7
Usuario <sergiocalvogonzalez7@gmail.com>
2024-10-18 16:44:32 +0200

    Subo la documentación de la clase game en java

--- irrgarten/src/irrgarten/Game.java
+++ irrgarten/src/irrgarten/Game.java
@@ -6,19 +6,33 @@ package irrgarten;
 
 import java.util.ArrayList;
 /**
- *
- * @author usuario
+ * Clase Game. Esta clase contiene la implementación de la variable game y sus 
+ * funciones básicas para la utilización de la clase
+ * 
+ * @author Miguel Ángel Luque Gómez
+ * correo: e.mangelluqg@go.ugr.es
+ * 
+ * @author Sergio Calvo González
+ * correo: e.sergiocg10@go.ugr.es
+ * 
  */
 public class Game {
-    private static int MAX_ROUNDS = 10;
-    private int currentPlayerIndex;
-    private String log;
+    private static int MAX_ROUNDS = 10; //Número de rondas máximas que se van a jugar
+    private int currentPlayerIndex;     //Indice del jugador actual
+    private String log;                 //Variable donde almacenaremos los sucesos dentro de la ronda actual
     
-    private ArrayList<Player> players = new ArrayList<>();
-    private ArrayList<Monster> monsters = new ArrayList<>();
-    private Player currentPlayer;
-    private Labyrinth labyrinth;
+    private ArrayList<Player> players = new ArrayList<>();      //Variable donde almacenaremos la información de los jugadores
+    private ArrayList<Monster> monsters = new ArrayList<>();    //Variable donde almacenaremos la información de los monstruos
+    private Player currentPlayer;                               //Variable que almacena los valores del jugador actual
+    private Labyrinth labyrinth;                                //Variable donde almacenaremos la información de todo el laberinto
     
+    /**
+     * Constructor por parámetros de la clase Game.
+     * Construye una variable Game con nPlayers como número de jugadores
+     * Los valores de inteligencia y fuerza se atribuyen de forma aleatoria
+     * 
+     * @param nplayers Número de jugadores que jugarán al juego
+     */
     Game(int nplayers){
         for(int i =0; i < nplayers; i++){
             // Al valor de 1 en ascii (49), le sumamos i para obtener el valor de jugador en forma de char
@@ -28,6 +42,11 @@ public class Game {
         }
     }
     
+    /**
+     * Funcion finished. Indica si el juego ha terminado o no. (Hay un ganador)
+     *  
+     * @return True si hay ganador, false si no
+     */
     public boolean finished(){
         return labyrinth.haveAWinner();
     }
@@ -35,6 +54,12 @@ public class Game {
     //Practica 3
     //public boolean nextStep(Directions preferredDirection)
     
+    /**
+     * Funcion getGameState. Almacena los valores del juego actual y los devuelve.
+     *  
+     * @return Variable GameState que contiene el laberinto, todos los jugadores, todos los monstruos
+     *          el índice de jugador actual, si el juego ha terminado, y el log de la ronda.
+     */
     public GameState getGameState(){
         String  allPlayers = "";
         String allMonsters = "";
@@ -52,6 +77,10 @@ public class Game {
     //Cómo configuramos el laberinto?
     //private void configureLabyrinth()
     
+    /**
+     * Funcion nextPlayer. La funcion almacena los cambios en el jugador actual
+     * y pasa al siguiente jugador. 
+     */
     private void nextPlayer(){
         //Actualizar los dos atributos del jugador current?
         players.set(currentPlayerIndex, currentPlayer);
@@ -76,30 +105,53 @@ public class Game {
     //Practica 3
     //private void manageResurrection()
     
+    /**
+     * Funcion logPlayerWon. Concatena a la variable log que ha ganado el combate el jugador
+     */
     private void logPlayerWon(){
         log += "El jugador ha ganado el combate\n";
     }
     
+    /**
+     * Funcion logMonsterWon. Concatena a la variable log que ha ganado el combate el monstruo
+     */
     private void logMonsterWon(){
         log += "El monstruo ha ganado el combate\n";
     }
     
+    /**
+     * Funcion logResurrected. Concatena a la variable log que el jugador ha resucitado
+     */
     private void logResurrected(){
         log += "El jugador ha resucitado\n";
     }
     
+    /**
+     * Funcion logPlayerWon. Concatena a la variable log que el jugador ha perdido su turno
+     */
     private void logPlayerSkipTurn(){
         log += "El jugador ha perdido turno por estar muerto\n";
     }
     
-    private void logPlayerNoOrderns(){
+    /**
+     * Funcion logPlayerNoOrders. Concatena a la variable log que 
+     * el jugador no pudo seguir las instrucciones 
+     */
+    private void logPlayerNoOrders(){
         log += "El jugador no ha podido seguir las instrucciones\n";
     }
     
+    /**
+     * Funcion logNoMonster. Concatena a la variable log que el jugador se ha movido
+     * a una celda sin monstruo o no ha podido moverse.
+     */
     private void logNoMonster(){
         log += "El jugador se ha movido a una celda vacía o no ha podido moverse\n";
     }
     
+    /**
+     * Funcion logRounds. Concatena a la variable log que han pasado x rondas de MAX_ROUNDS
+     */
     private void logRounds(int rounds, int max){
         log += "Se han producido " + rounds + " de " + max + "\n";
     }
