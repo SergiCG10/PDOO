ab95313b7098b21852e983e61ee2fba154453c05
Mangel <mangelluqueg@gmail.com>
2024-11-05 23:02:06 +0000

    sergio puton me has jodido con el pull xd

--- Irrgarten/lib/Game.rb
+++ Irrgarten/lib/Game.rb
@@ -8,23 +8,43 @@
 # correo e.sergiocg10@go.ugr.es
 
 require_relative 'Player'
-
+require_relative 'GameState'
 class Game
 	@@MAX_ROUNDS=10
-	@players=Array.new
 	@monsters=Array.new
 	@currentPlayer
 	@labyrinth
 	
 	def initialize (nplayers)
-		players.each do |player|
-			c=(char)(player+'1')
+		@players=Array.new(nplayers)
+		for i in 0..nplayers-1
+			c=(i+1).to_s
 			p=Player.new(c,Dice.randomIntelligence,Dice.randomStrength)	
-			players.push(p)
+			@players[i]=p
+			#puts @players[i].toString
 		end
 		@currentPlayerIndex=0
 		@log=""
 	end
+	
+	def finished
+		@labyrinth.haveAWinner
+	end
+	
+	def getGameState
+		allPlayers=""
+		allMonsters=""
+		@players.each do |player|
+			allPlayers+=player.toString
+		end
+		@monsters.each do |monster|
+			allMonsters+=monster.toString
+		end
+		return Gamestate.new(@labyrinth.toString, allPlayers, allMonsters, @currentPlayer)
+	end
 
 
 end
+
+g=Game.new(3)
+
